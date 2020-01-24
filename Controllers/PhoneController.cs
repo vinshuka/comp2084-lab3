54237fb6e2f532bdc99884653a1384c80b9a74dc
Vinshuka <melissa.ann.christie@gmail.com>
2020-01-23 20:32:14 -0500

    Add project files.

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab3.Models;
+
+
+namespace Lab3.Controllers
+{
+    public class PhoneController : Controller
+    {
+        public ActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone {phoneId = 1, phoneName = "Nexsus", manufacturer = "Samsung", MSRP = 399.00, screenSize = 5.2, dateReleased = new DateTime(2009, 6, 28) },
+                new Phone {phoneId = 2, phoneName = "Idol 3", manufacturer = "Alcaltel", MSRP = 299.00, screenSize = 6.2, dateReleased = new DateTime(2013, 10, 14) },
+                new Phone {phoneId = 3, phoneName = "Iphone 9S", manufacturer = "Apple", MSRP = 899.00, screenSize = 6.4, dateReleased = new DateTime(2018, 8, 3) },
+                new Phone {phoneId = 4, phoneName = "Note 10", manufacturer = "Samsung", MSRP = 1199.00, screenSize = 8.2, dateReleased = new DateTime(2019, 4, 22) }
+            };
+
+            return View(phoneList);
+        }
+
+   
+}
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab3.Models
+{
+    public class Phone
+    {
+
+        public Phone()
+        {
+            phoneId = 0;
+            phoneName = "";
+            manufacturer = "";
+            MSRP = 0.0;
+            screenSize = 0.0;
+            dateReleased = new DateTime(2000, 1, 1);
+
+        }
+
+        public int phoneId { get; set; }
+        public string phoneName { get; set; }
+        public string manufacturer { get; set; }
+        public double MSRP { get; set; }
+        public double screenSize { get; set; }
+        public DateTime dateReleased { get; set; }
+
+    }
+}
